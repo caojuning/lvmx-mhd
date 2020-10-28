1d899afebef21fee534b12111816e0390b164381
caojun <834634377@qq.com>
2020-10-28 20:31:35 +0800

    限制最大540

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="page-home">
+    <header class="index-header">
+    <a href="mine.html?cpid=0">
+       <div class="header-user">
+             <div class="user-btn"></div>
 
-<h1>首页页面</h1>
+       </div>
+    </a>
+    <div class="header-logo"></div>
+    <a href="search.html?cpid=0">
+       <div class="header-search"></div>
+    </a>
+</header>
+
   </div>
 </template>
 
@@ -12,3 +23,37 @@ export default {
 
 }
 </script>
+<style lang="scss" scoped>
+.page-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  .index-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    height: 44px;
+    box-sizing: border-box;
+    .user-btn {
+      width: 25px;
+      height: 25px;
+      background: url('../../assets/icon/user-btn.png');
+      background-size: 100%;
+    }
+    .header-logo {
+      width: 92px;
+      height: 28px;
+      background: url('../../assets/logo.png') no-repeat;
+      background-size: 100%;
+    }
+    .header-search {
+      width: 25px;
+      height: 25px;
+      background: url('../../assets/icon/search.png');
+      background-size: 100%;
+    }
+
+  }
+}
+</style>
